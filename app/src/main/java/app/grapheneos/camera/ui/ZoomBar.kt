a9b9f8bdee111787859f38f650a4c39418f82cc9
MHShetty <shettymohitn@gmail.com>
2021-09-24 15:39:33 +0530

    Integrate added resources with the zoom bar

--- app/src/main/java/app/grapheneos/camera/ui/ZoomBar.kt
+++ app/src/main/java/app/grapheneos/camera/ui/ZoomBar.kt
@@ -7,6 +7,18 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatSeekBar
+import android.graphics.drawable.BitmapDrawable
+
+import android.graphics.Bitmap
+
+import android.widget.TextView
+
+import android.graphics.drawable.Drawable
+import android.view.View
+
+import android.view.LayoutInflater
+import app.grapheneos.camera.R
+
 
 class ZoomBar : AppCompatSeekBar {
     constructor(context: Context) : super(context)
@@ -16,10 +28,32 @@ class ZoomBar : AppCompatSeekBar {
         defStyle
     )
 
+    @SuppressLint("InflateParams")
+    private var thumbView: View = LayoutInflater.from(context)
+        .inflate(R.layout.zoom_bar_thumb, null, false)
+
+    init {
+        thumb = getThumb(0)
+    }
+
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(h, w, oldh, oldw)
+    }
+
+    private fun getThumb(progress: Int): Drawable {
+        (thumbView.findViewById(R.id.progress) as TextView).text = progress.toString()
+        thumbView.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+        val bitmap = Bitmap.createBitmap(
+            thumbView.measuredWidth,
+            thumbView.measuredHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        thumbView.layout(0, 0, thumbView.measuredWidth, thumbView.measuredHeight)
+        thumbView.draw(canvas)
+        return BitmapDrawable(resources, bitmap)
     }
 
     @Synchronized
@@ -42,6 +76,7 @@ class ZoomBar : AppCompatSeekBar {
         when (event.action) {
             MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
                 progress = max - (max * event.y / height).toInt()
+                thumb = getThumb(progress)
                 Log.i("Progress", progress.toString() + "")
                 onSizeChanged(width, height, 0, 0)
             }
