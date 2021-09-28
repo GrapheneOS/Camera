02c97a52de2aaaae31fb880d90473f6911bf061d
MHShetty <shettymohitn@gmail.com>
2021-09-29 03:29:49 +0530

    Make overlay box 60% of the smaller dimension

--- app/src/main/java/app/grapheneos/camera/ui/QROverlay.kt
+++ app/src/main/java/app/grapheneos/camera/ui/QROverlay.kt
@@ -31,11 +31,12 @@ class QROverlay(context: Context, attrs: AttributeSet) : View(context, attrs) {
     fun setViewFinder() {
         val overlayWidth = width.toFloat()
         val overlayHeight = height.toFloat()
-        val boxWidth = overlayWidth * 80 / 100
-        val boxHeight = overlayHeight * 36 / 100
+
+        val boxDim = overlayHeight.coerceAtMost(overlayWidth) * 0.6f
+
         val cx = overlayWidth / 2
         val cy = overlayHeight / 2
-        boxRect = RectF(cx - boxWidth / 2, cy - boxHeight / 2, cx + boxWidth / 2, cy + boxHeight / 2)
+        boxRect = RectF(cx - boxDim / 2, cy - boxDim / 2, cx + boxDim / 2, cy + boxDim / 2)
 
         invalidate()
     }
