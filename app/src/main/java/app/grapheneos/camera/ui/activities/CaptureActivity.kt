6d0d4f565937775c4527387bfc5c997d2d92fb37
MHShetty <shettymohitn@gmail.com>
2021-09-28 17:44:54 +0530

    Create CaptureActivity from MainActivity (to deal with image/video capture intents separately)

---
+++ app/src/main/java/app/grapheneos/camera/ui/activities/CaptureActivity.kt
@@ -0,0 +1,10 @@
+package app.grapheneos.camera.ui.activities
+
+import android.os.Bundle
+
+class CaptureActivity: MainActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
