854bf24fab36432f1bdb62820e89cb2d4008d7c0
MHShetty <shettymohitn@gmail.com>
2021-10-05 13:57:58 +0530

    Remove unused import in CamConfig.kt

--- app/src/main/java/app/grapheneos/camera/CamConfig.kt
+++ app/src/main/java/app/grapheneos/camera/CamConfig.kt
@@ -16,7 +16,6 @@ import androidx.core.content.ContextCompat
 import app.grapheneos.camera.ui.activities.MainActivity
 import java.io.File
 import java.util.concurrent.ExecutionException
-import java.util.concurrent.TimeUnit
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.Animation
 
