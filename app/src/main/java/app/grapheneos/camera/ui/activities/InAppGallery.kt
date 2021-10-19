c5b1f9bb577c803a865fd3c17d9d7cc1b178f462
MHShetty <shettymohitn@gmail.com>
2021-10-19 14:22:40 +0530

    Add back button to gallery

--- app/src/main/java/app/grapheneos/camera/ui/activities/InAppGallery.kt
+++ app/src/main/java/app/grapheneos/camera/ui/activities/InAppGallery.kt
@@ -1,6 +1,7 @@
 package app.grapheneos.camera.ui.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import app.grapheneos.camera.GSlideTransformer
@@ -42,5 +43,10 @@ class InAppGallery: AppCompatActivity() {
             this,
             mediaFiles
         )
+
+        val backButton : ImageView = findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
