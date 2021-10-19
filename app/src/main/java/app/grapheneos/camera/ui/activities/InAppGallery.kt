4add5aef94e0451f1edf0e5d2c9a965585a4d763
MHShetty <shettymohitn@gmail.com>
2021-10-19 20:05:55 +0530

    Add functional details icon to gallery

--- app/src/main/java/app/grapheneos/camera/ui/activities/InAppGallery.kt
+++ app/src/main/java/app/grapheneos/camera/ui/activities/InAppGallery.kt
@@ -1,15 +1,20 @@
 package app.grapheneos.camera.ui.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import app.grapheneos.camera.GSlideTransformer
 import app.grapheneos.camera.GallerySliderAdapter
-import app.grapheneos.camera.R
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
+
+import androidx.appcompat.app.AlertDialog
+import app.grapheneos.camera.R
+import java.text.Format
+import java.text.SimpleDateFormat
 
 class InAppGallery: AppCompatActivity() {
 
@@ -47,6 +52,57 @@ class InAppGallery: AppCompatActivity() {
         val backButton : ImageView = findViewById(R.id.back_button)
         backButton.setOnClickListener {
             finish()
+        }
+
+        val fIButton : ImageView = findViewById(R.id.file_info)
+        fIButton.setOnClickListener {
+            val file = getCurrentFile()
+
+            val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this)
+
+            alertDialog.setTitle("File Details")
+
+            val detailsBuilder = StringBuilder()
+
+            detailsBuilder.append("File Name: \n")
+            detailsBuilder.append(file.name)
+            detailsBuilder.append("\n\n")
+
+            detailsBuilder.append("File Path: \n")
+            detailsBuilder.append(file.absolutePath)
+            detailsBuilder.append("\n\n")
+
+            detailsBuilder.append("File Size: \n")
+            detailsBuilder.append(
+                String.format(
+                    "%.2f",
+                    (file.length() / (1024f * 1024f))
+                )
+            )
+            detailsBuilder.append(" mb\n\n")
+
+            detailsBuilder.append("File Created On: \n")
+            detailsBuilder.append(convertTime(file.lastModified()))
+
+            alertDialog.setMessage(detailsBuilder)
+
+            alertDialog.setPositiveButton("Ok", null)
+            alertDialog.show()
+        }
+    }
+
+    private fun getCurrentFile() : File {
+        val i = gallerySlider.currentItem
+        return mediaFiles[i]
+    }
+
+    companion object {
+        @SuppressLint("SimpleDateFormat")
+        fun convertTime(time: Long): String {
+            val date = Date(time)
+            val format: Format =
+                SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+            return format.format(date)
         }
     }
 }
