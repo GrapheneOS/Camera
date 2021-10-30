4074063e888e0297205791ccdee84bd123f669f3
MHShetty <shettymohitn@gmail.com>
2021-10-31 03:11:43 +0530

    Make SecureCaptureActivity visible/accessible in lock screen

--- app/src/main/java/app/grapheneos/camera/ui/activities/SecureCaptureActivity.kt
+++ app/src/main/java/app/grapheneos/camera/ui/activities/SecureCaptureActivity.kt
@@ -1,3 +1,12 @@
 package app.grapheneos.camera.ui.activities
 
-class SecureCaptureActivity : CaptureActivity()
+import android.os.Bundle
+
+class SecureCaptureActivity : CaptureActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setShowWhenLocked(true)
+        setTurnScreenOn(true)
+    }
+}
