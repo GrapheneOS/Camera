4f9d6f637766d1351804c4ca460102712bf00399
flawedworld <flawedworld@flawed.world>
2021-10-31 00:19:18 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,14 +11,14 @@ buildscript {
 }
 
 plugins {
-  id "org.sonarqube" version "3.3"
+  id("org.sonarqube") version "3.3"
 }
 
 sonarqube {
   properties {
-    property "sonar.projectKey", "flawedworld_grapheneos-camera"
-    property "sonar.organization", "flawedworld"
-    property "sonar.host.url", "https://sonarcloud.io"
+    property("sonar.projectKey", "flawedworld_grapheneos-camera")
+    property("sonar.organization", "flawedworld")
+    property("sonar.host.url", "https://sonarcloud.io")
   }
 }
 
