da4f055ddbe9453ea2899cec4d7fe9d36e283ac0
MHShetty <shettymohitn@gmail.com>
2021-11-06 20:41:46 +0530

    Properly implement actions for InAppGallery

--- app/src/main/java/app/grapheneos/camera/ui/activities/InAppGallery.kt
+++ app/src/main/java/app/grapheneos/camera/ui/activities/InAppGallery.kt
@@ -6,12 +6,15 @@ import android.app.AlertDialog
 import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.widget.ImageView
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import androidx.exifinterface.media.ExifInterface
@@ -34,9 +37,10 @@ class InAppGallery : AppCompatActivity() {
     private val mediaUris: ArrayList<Uri> = arrayListOf()
     private var snackBar : Snackbar? = null
 
-    private fun getCurrentUri(): Uri {
-        return (gallerySlider.adapter as GallerySliderAdapter).getCurrentUri()
-    }
+    private val isSecureMode : Boolean
+        get() {
+            return intent.extras?.containsKey("fileSP") == true
+        }
 
     private val editIntentLauncher =
         registerForActivityResult(StartActivityForResult())
@@ -68,15 +72,280 @@ class InAppGallery : AppCompatActivity() {
             }
         }
 
-    private val isSecureMode : Boolean
-        get() {
-            return intent.extras?.containsKey("fileSP") == true
+    companion object {
+        @SuppressLint("SimpleDateFormat")
+        fun convertTime(time: Long): String {
+
+            val date = Date(time)
+            val format: Format = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+            return format.format(date)
         }
+
+        @SuppressLint("SimpleDateFormat")
+        fun convertTime(time: String): String {
+            val dateFormat = SimpleDateFormat("yyyyMMdd'T'hhmmss.SSS'Z'")
+            dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+            val parsedDate = dateFormat.parse(time)
+            return convertTime(parsedDate?.time ?: 0)
+        }
+
+    }
+
+    private fun getCurrentUri(): Uri {
+        return (gallerySlider.adapter as GallerySliderAdapter).getCurrentUri()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.gallery, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.edit_icon -> {
+                editCurrentMedia()
+                true
+            }
+
+            R.id.delete_icon -> {
+                deleteCurrentMedia()
+                true
+            }
+
+            R.id.info -> {
+                showCurrentMediaDetails()
+                true
+            }
+
+            R.id.share_icon -> {
+                shareCurrentMedia()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun editCurrentMedia() {
+        if (isSecureMode){
+            showMessage(
+                "Editing images in secure mode is not allowed."
+            )
+            return
+        }
+
+        val mediaUri = getCurrentUri()
+
+        val editIntent = Intent(Intent.ACTION_EDIT)
+
+        editIntent.putExtra(Intent.EXTRA_STREAM, mediaUri)
+        editIntent.data = mediaUri
+
+        editIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        editIntentLauncher.launch(
+            Intent.createChooser(editIntent, "Edit Image")
+        )
+    }
+
+    private fun deleteCurrentMedia() {
+
+        val mediaUri = getCurrentUri()
+
+        AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert)
+            .setTitle("Are you sure?")
+            .setMessage("Do you really want to delete this file?")
+            .setPositiveButton("Delete") { _, _ ->
+
+                val res = contentResolver.delete(
+                    mediaUri,
+                    null,
+                    null
+                ) == 1
+
+                if (res) {
+
+                    showMessage(
+                        "File deleted successfully"
+                    )
+
+                    (gallerySlider.adapter as GallerySliderAdapter).removeUri(mediaUri)
+
+                } else {
+                    showMessage(
+                        "Unable to delete this file"
+                    )
+                }
+            }
+            .setNegativeButton("Cancel", null).show()
+
+    }
+
+    private fun showCurrentMediaDetails() {
+        val mediaUri = getCurrentUri()
+
+        val mediaCursor = contentResolver.query(
+            mediaUri,
+            arrayOf(
+                MediaStore.MediaColumns.RELATIVE_PATH,
+                MediaStore.MediaColumns.DISPLAY_NAME,
+                MediaStore.MediaColumns.SIZE
+            ),
+            null,
+            null,
+        )
+
+        if (mediaCursor?.moveToFirst() != true) {
+            showMessage(
+                "An unexpected error occurred"
+            )
+
+            mediaCursor?.close()
+            return
+        }
+
+        val relativePath = mediaCursor.getString(0)
+        val fileName = mediaCursor.getString(1)
+        val size = mediaCursor.getInt(2)
+
+        mediaCursor.close()
+
+        val dateAdded : String?
+        val dateModified : String?
+
+        if (VideoCapturer.isVideo(mediaUri)) {
+
+            val mediaMetadataRetriever = MediaMetadataRetriever()
+            mediaMetadataRetriever.setDataSource(
+                this,
+                mediaUri
+            )
+
+            val date =
+                convertTime(
+                    mediaMetadataRetriever.extractMetadata(
+                        MediaMetadataRetriever.METADATA_KEY_DATE
+                    )!!
+                )
+
+            dateAdded = date
+            dateModified = date
+
+        } else {
+            val iStream = contentResolver.openInputStream(
+                mediaUri
+            )
+            val eInterface = ExifInterface(iStream!!)
+
+            dateAdded = eInterface.getAttribute(
+                "DateTimeOriginal"
+            )
+
+            dateModified = eInterface.getAttribute(
+                "DateTime"
+            )
+
+            iStream.close()
+        }
+
+
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert)
+
+        alertDialog.setTitle("File Details")
+
+        val detailsBuilder = StringBuilder()
+
+        detailsBuilder.append("\nFile Name: \n")
+        detailsBuilder.append(fileName)
+        detailsBuilder.append("\n\n")
+
+        detailsBuilder.append("File Path: \n")
+        detailsBuilder.append("$relativePath$fileName")
+        detailsBuilder.append("\n\n")
+
+        detailsBuilder.append("File Size: \n")
+        if(size==0){
+            detailsBuilder.append("Loading...")
+        } else {
+            detailsBuilder.append(
+                String.format(
+                    "%.2f",
+                    (size / (1024f * 1024f))
+                )
+            )
+            detailsBuilder.append(" mb")
+        }
+
+        detailsBuilder.append("\n\n")
+
+        Log.i("TAG", "Date added: $dateAdded")
+
+        detailsBuilder.append("File Created On: \n")
+        if(dateAdded==null){
+            detailsBuilder.append("Not found")
+        } else {
+            detailsBuilder.append(dateAdded)
+        }
+
+        detailsBuilder.append("\n\n")
+
+        detailsBuilder.append("Last Modified On: \n")
+        if(dateModified==null){
+            detailsBuilder.append("Not found")
+        } else {
+            detailsBuilder.append(dateModified)
+        }
+
+        alertDialog.setMessage(detailsBuilder)
+
+        alertDialog.setPositiveButton("Ok", null)
+
+
+        alertDialog.show()
+    }
+
+    private fun shareCurrentMedia() {
+
+        if (isSecureMode) {
+            showMessage(
+                "Sharing images in secure mode is not allowed."
+            )
+            return
+        }
+
+        val mediaUri = getCurrentUri()
+
+        val share = Intent(Intent.ACTION_SEND)
+        share.putExtra(Intent.EXTRA_STREAM, mediaUri)
+        share.data = mediaUri
+        share.type = if (VideoCapturer.isVideo(mediaUri)) {
+            "video/*"
+        } else {
+            "image/*"
+        }
+        share.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+
+        startActivity(
+            Intent.createChooser(share, "Share Image")
+        )
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gallery)
+
+        supportActionBar?.let {
+            it.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            it.setDisplayShowTitleEnabled(false)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
 
         gallerySlider = findViewById(R.id.gallery_slider)
         gallerySlider.setPageTransformer(GSlideTransformer())
@@ -84,20 +353,15 @@ class InAppGallery : AppCompatActivity() {
         val showVideosOnly = intent.extras?.getBoolean("show_videos_only")!!
 
         if (isSecureMode) {
-
             val spName = intent.extras?.getString("fileSP")
-
             val sp = getSharedPreferences(spName, Context.MODE_PRIVATE)
-
             val filePaths = sp.getStringSet("filePaths", emptySet())!!
-
             val mediaFileArray: Array<Uri> =
                 filePaths.stream().map { Uri.parse(it) }.toArray { length ->
                     arrayOfNulls<Uri>(length)
                 }
 
             mediaUris.addAll(mediaFileArray)
-
         } else {
 
             val cursor = contentResolver.query(
@@ -141,230 +405,16 @@ class InAppGallery : AppCompatActivity() {
             }
         }
 
-            // Close gallery if no files are present
-            if (mediaUris.isEmpty()) {
-                showMessage(
-                    "Please capture a photo/video before trying to view" +
-                            " them."
-                )
-                finish()
-            }
+        // Close gallery if no files are present
+        if (mediaUris.isEmpty()) {
+            showMessage(
+                "Please capture a photo/video before trying to view" +
+                        " them."
+            )
+            finish()
+        }
 
-            gallerySlider.adapter = GallerySliderAdapter(this, mediaUris)
-
-            val backButton: ImageView = findViewById(R.id.back_button)
-            backButton.setOnClickListener {
-                finish()
-            }
-
-            val fIButton: ImageView = findViewById(R.id.file_info)
-            fIButton.setOnClickListener {
-
-                val mediaUri = getCurrentUri()
-
-                val mediaCursor = contentResolver.query(
-                    mediaUri,
-                    arrayOf(
-                        MediaStore.MediaColumns.RELATIVE_PATH,
-                        MediaStore.MediaColumns.DISPLAY_NAME,
-                        MediaStore.MediaColumns.SIZE
-                    ),
-                    null,
-                    null,
-                )
-
-                if (mediaCursor?.moveToFirst() != true) {
-                    showMessage(
-                        "An unexpected error occurred"
-                    )
-
-                    mediaCursor?.close()
-                    return@setOnClickListener
-                }
-
-                val relativePath = mediaCursor.getString(0)
-                val fileName = mediaCursor.getString(1)
-                val size = mediaCursor.getInt(2)
-
-                mediaCursor.close()
-
-                val dateAdded : String?
-                val dateModified : String?
-
-                if (VideoCapturer.isVideo(mediaUri)) {
-
-                    val mediaMetadataRetriever = MediaMetadataRetriever()
-                    mediaMetadataRetriever.setDataSource(
-                        this,
-                        mediaUri
-                    )
-
-                    val date =
-                        convertTime(
-                            mediaMetadataRetriever.extractMetadata(
-                                MediaMetadataRetriever.METADATA_KEY_DATE
-                            )!!
-                        )
-
-                    dateAdded = date
-                    dateModified = date
-
-                } else {
-                    val iStream = contentResolver.openInputStream(
-                        mediaUri
-                    )
-                    val eInterface = ExifInterface(iStream!!)
-
-                    dateAdded = eInterface.getAttribute(
-                        "DateTimeOriginal"
-                    )
-
-                    dateModified = eInterface.getAttribute(
-                        "DateTime"
-                    )
-
-                    iStream.close()
-                }
-
-
-                val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert)
-
-                alertDialog.setTitle("File Details")
-
-                val detailsBuilder = StringBuilder()
-
-                detailsBuilder.append("\nFile Name: \n")
-                detailsBuilder.append(fileName)
-                detailsBuilder.append("\n\n")
-
-                detailsBuilder.append("File Path: \n")
-                detailsBuilder.append("$relativePath$fileName")
-                detailsBuilder.append("\n\n")
-
-                detailsBuilder.append("File Size: \n")
-                if(size==0){
-                    detailsBuilder.append("Loading...")
-                } else {
-                    detailsBuilder.append(
-                        String.format(
-                            "%.2f",
-                            (size / (1024f * 1024f))
-                        )
-                    )
-                    detailsBuilder.append(" mb")
-                }
-
-                detailsBuilder.append("\n\n")
-
-                Log.i("TAG", "Date added: $dateAdded")
-
-                detailsBuilder.append("File Created On: \n")
-                if(dateAdded==null){
-                    detailsBuilder.append("Not found")
-                } else {
-                    detailsBuilder.append(dateAdded)
-                }
-
-                detailsBuilder.append("\n\n")
-
-                detailsBuilder.append("Last Modified On: \n")
-                if(dateModified==null){
-                    detailsBuilder.append("Not found")
-                } else {
-                    detailsBuilder.append(dateModified)
-                }
-
-                alertDialog.setMessage(detailsBuilder)
-
-                alertDialog.setPositiveButton("Ok", null)
-
-
-                alertDialog.show()
-            }
-
-            val shareIcon: ImageView = findViewById(R.id.share_icon)
-            shareIcon.setOnClickListener {
-
-                if (isSecureMode) {
-                    showMessage(
-                        "Sharing images in secure mode is not allowed."
-                    )
-                    return@setOnClickListener
-                }
-
-                val mediaUri = getCurrentUri()
-
-                val share = Intent(Intent.ACTION_SEND)
-                share.putExtra(Intent.EXTRA_STREAM, mediaUri)
-                share.data = mediaUri
-                share.type = if (VideoCapturer.isVideo(mediaUri)) {
-                    "video/*"
-                } else {
-                    "image/*"
-                }
-                share.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-
-                startActivity(
-                    Intent.createChooser(share, "Share Image")
-                )
-            }
-
-            val deleteIcon: ImageView = findViewById(R.id.delete_icon)
-            deleteIcon.setOnClickListener {
-
-                val mediaUri = getCurrentUri()
-
-                AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert)
-                    .setTitle("Are you sure?")
-                    .setMessage("Do you really want to delete this file?")
-                    .setPositiveButton("Delete") { _, _ ->
-
-                        val res = contentResolver.delete(
-                            mediaUri,
-                            null,
-                            null
-                        ) == 1
-
-                        if (res) {
-
-                            showMessage(
-                                "File deleted successfully"
-                            )
-
-                            (gallerySlider.adapter as GallerySliderAdapter).removeUri(mediaUri)
-
-                        } else {
-                            showMessage(
-                                "Unable to delete this file"
-                            )
-                        }
-                    }
-                    .setNegativeButton("Cancel", null).show()
-
-            }
-
-            val editIcon: ImageView = findViewById(R.id.edit_icon)
-            editIcon.setOnClickListener {
-
-                if (isSecureMode){
-                    showMessage(
-                        "Editing images in secure mode is not allowed."
-                    )
-                    return@setOnClickListener
-                }
-
-                val mediaUri = getCurrentUri()
-
-                val editIntent = Intent(Intent.ACTION_EDIT)
-
-                editIntent.putExtra(Intent.EXTRA_STREAM, mediaUri)
-                editIntent.data = mediaUri
-
-                editIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-                editIntentLauncher.launch(
-                    Intent.createChooser(editIntent, "Edit Image")
-                )
-            }
+        gallerySlider.adapter = GallerySliderAdapter(this, mediaUris)
 
         snackBar = Snackbar.make(
             gallerySlider,
@@ -374,52 +424,8 @@ class InAppGallery : AppCompatActivity() {
 
     }
 
-    //    companion object {
-//        @SuppressLint("SimpleDateFormat")
-//        fun convertTime(time: Long): String {
-//            val date = Date(time)
-//            val format: Format =
-//                SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-//            return format.format(date)
-//        }
-//
-//        fun getCreationTimestamp(file: File): Long {
-//            val attr: BasicFileAttributes = Files.readAttributes(
-//                file.toPath(),
-//                BasicFileAttributes::class.java
-//            )
-//            return attr.creationTime().toMillis()
-//        }
-//    }
-
     fun showMessage(msg: String) {
         snackBar?.setText(msg)
         snackBar?.show()
-    }
-
-    companion object {
-        @SuppressLint("SimpleDateFormat")
-        fun convertTime(time: Long): String {
-
-            Log.i("TAG", "convertTime: $time")
-
-            val date = Date(time)
-            val format: Format = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-            val res = format.format(date)
-
-            Log.i("TAG", "convertTime: $res")
-
-            return res
-        }
-
-        @SuppressLint("SimpleDateFormat")
-        fun convertTime(time: String): String {
-
-            val dateFormat = SimpleDateFormat("yyyyMMdd'T'hhmmss.SSS'Z'")
-            dateFormat.timeZone = TimeZone.getTimeZone("UTC")
-            val parsedDate = dateFormat.parse(time)
-            return convertTime(parsedDate?.time ?: 0)
-        }
-
     }
 }
