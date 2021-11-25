888d6891143e4e2b1b911ccce4a1a136a846ced5
MHShetty <shettymohitn@gmail.com>
2021-11-25 16:30:57 +0530

    Add class for 'More Settings' activity

---
+++ app/src/main/java/app/grapheneos/camera/ui/activities/MoreSettings.kt
@@ -0,0 +1,14 @@
+package app.grapheneos.camera.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.grapheneos.camera.R
+
+class MoreSettings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.more_settings)
+    }
+
+}
