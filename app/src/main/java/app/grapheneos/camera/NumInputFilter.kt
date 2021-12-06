7598476461e1c8b08965610249fa2941ab5e5098
MHShetty <shettymohitn@gmail.com>
2021-12-06 20:44:17 +0530

    Add filter class for photo quality field

---
+++ app/src/main/java/app/grapheneos/camera/NumInputFilter.kt
@@ -0,0 +1,33 @@
+package app.grapheneos.camera
+
+import android.text.InputFilter
+import android.text.Spanned
+import app.grapheneos.camera.ui.activities.MoreSettings
+
+class NumInputFilter(private val mActivity : MoreSettings) : InputFilter {
+
+    override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence? {
+        try {
+            val input = (dest.subSequence(0, dstart).toString() + source + dest.subSequence(dend, dest.length)).toInt()
+            if (isInRange(input)) {
+                return null
+            } else {
+                mActivity.showMessage(
+                    "Photo quality can only be between $min and $max"
+                )
+            }
+        } catch (e: NumberFormatException) {
+            e.printStackTrace()
+        }
+        return ""
+    }
+
+    private fun isInRange(value: Int): Boolean {
+        return value in min..max
+    }
+
+    companion object {
+        private const val min = 0
+        private const val max = 100
+    }
+}
