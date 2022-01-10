2ee056fb197b32c1e4ee1f38fbfdb74f83a5ece9
Daniel Micay <danielmicay@gmail.com>
2022-01-10 02:30:59 -0500

    increment version to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "app.grapheneos.camera"
         minSdk = 29
         targetSdk = 31
-        versionCode = 8
+        versionCode = 9
         versionName = versionCode.toString()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
