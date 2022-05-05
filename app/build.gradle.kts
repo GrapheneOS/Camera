7bacd03f2c22e6748e827caec79842ca2289e9ce
MHShetty <shettymohitn@gmail.com>
2022-05-05 08:05:34 +0530

    Update material library dependency to 1.6.0 (from 1.5.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.exifinterface:exifinterface:1.3.3")
 
