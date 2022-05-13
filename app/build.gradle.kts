ff6dc0e9a3d8159719a4c789c8daf7caeceb94d6
Daniel Micay <danielmicay@gmail.com>
2022-05-13 19:44:18 -0400

    increment version to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "app.grapheneos.camera"
         minSdk = 29
         targetSdk = 32
-        versionCode = 36
+        versionCode = 37
         versionName = versionCode.toString()
     }
 
