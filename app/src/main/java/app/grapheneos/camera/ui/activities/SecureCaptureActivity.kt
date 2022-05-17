dd92dd93725224e72cd441797520587f4810940c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-05-17 16:26:40 +0300

    improve implementation of lock-screen-accessible ("secure") activities
    Previously, each instance of a secure activity produced multiple non-removable
    SharedPreferences files, and ignored settings that were changed in the "regular"
    mode.
    
    Instead, use in-memory EphemeralSharedPrefs that are copied from "real" prefs,
    but never modify them.
    
    Note that image saving / gallery code is handled in subsequent commits.

--- app/src/main/java/app/grapheneos/camera/ui/activities/SecureCaptureActivity.kt
+++ app/src/main/java/app/grapheneos/camera/ui/activities/SecureCaptureActivity.kt
@@ -1,14 +1,13 @@
 package app.grapheneos.camera.ui.activities
 
-import android.os.Bundle
+import android.content.SharedPreferences
+import app.grapheneos.camera.util.EphemeralSharedPrefsNamespace
+import app.grapheneos.camera.util.getPrefs
 
-class SecureCaptureActivity : CaptureActivity() {
+class SecureCaptureActivity : CaptureActivity(), SecureActivity {
+    val ephemeralPrefsNamespace = EphemeralSharedPrefsNamespace()
 
-    var openedActivityAt = SecureMainActivity.DEFAULT_OPENED_AT_TIMESTAMP
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        openedActivityAt = System.currentTimeMillis()
+    override fun getSharedPreferences(name: String, mode: Int): SharedPreferences {
+        return ephemeralPrefsNamespace.getPrefs(this, name, mode, cloneOriginal = true)
     }
-
 }
