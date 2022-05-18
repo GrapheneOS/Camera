35206872576d52f5413c7c94496febcff6a61f3a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-05-18 17:11:03 +0300

    improve VideoCapturer
    - bugfix: output file descriptor wasn't being closed
    - writing to an Uri that is provided by VideoCaptureActivity wasn't handled
    properly (outputResults.outputUri is always null for FileDescriptorOutputOptions)
    - saving to SAF location didn't update the last captured item (my bug, I assumed
    that the code that used outputResults.outputUri was correct)
    - a prompt to reset storage location was shown when an external Uri that is
    passed by VideoCaptureActivity was inaccessible

--- app/src/main/java/app/grapheneos/camera/util/Utils.kt
+++ app/src/main/java/app/grapheneos/camera/util/Utils.kt
@@ -1,7 +1,20 @@
 package app.grapheneos.camera.util
 
+import android.content.Context
+import android.net.Uri
+import android.os.storage.StorageManager
+import android.provider.DocumentsContract
+import app.grapheneos.camera.CamConfig
+import app.grapheneos.camera.R
+import app.grapheneos.camera.capturer.DEFAULT_MEDIA_STORE_CAPTURE_PATH
+import app.grapheneos.camera.capturer.SAF_URI_HOST_EXTERNAL_STORAGE
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.RejectedExecutionException
+
+fun getTreeDocumentUri(treeUri: Uri): Uri {
+    val treeId = DocumentsContract.getTreeDocumentId(treeUri)
+    return DocumentsContract.buildDocumentUriUsingTree(treeUri, treeId)
+}
 
 fun ExecutorService.executeIfAlive(r: Runnable) {
     try {
