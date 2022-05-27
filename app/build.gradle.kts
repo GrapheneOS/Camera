14f536cbce7c3ca4ef3b5cbc581ea7d1dcadd6ec
Daniel Micay <danielmicay@gmail.com>
2022-05-26 20:49:12 -0400

    update constraintlayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.6.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("androidx.camera:camera-core:1.1.0-rc01")
     implementation("androidx.camera:camera-camera2:1.1.0-rc01")
