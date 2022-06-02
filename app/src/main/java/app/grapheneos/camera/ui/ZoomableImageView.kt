5aec329ec191da71d65cfc8b88de83f1227c0350
MHShetty <shettymohitn@gmail.com>
2022-06-02 16:51:53 +0530

    Remove unused imports

--- app/src/main/java/app/grapheneos/camera/ui/ZoomableImageView.kt
+++ app/src/main/java/app/grapheneos/camera/ui/ZoomableImageView.kt
@@ -9,7 +9,6 @@ import android.view.MotionEvent
 
 import android.graphics.PointF
 import android.util.AttributeSet
-import android.util.Log
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
 
 import androidx.appcompat.widget.AppCompatImageView
