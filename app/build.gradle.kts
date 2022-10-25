1c2fb9a0ad906f5a652bbd1c8e846207c7785640
smdyv <88971341+smdyv@users.noreply.github.com>
2022-10-25 20:42:54 +0200

    Bump material from 1.6.1 to 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     val cameraVersion = "1.2.0-beta02"
