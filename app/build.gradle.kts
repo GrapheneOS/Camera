b44fbe1604a19af67e16c22ab38f87c51e61eb98
Daniel Micay <danielmicay@gmail.com>
2023-02-01 20:48:20 -0500

    update Material library to 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.0")
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     val cameraVersion = "1.3.0-alpha02"
