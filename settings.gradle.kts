a5644901cef633e54685b6a1e9f4f050a77d7790
Patryk Mis <foss@patrykmis.com>
2023-04-19 22:00:34 +0200

    Update Gradle build scripts
    Signed-off-by: Patryk Mis <foss@patrykmis.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
