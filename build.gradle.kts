aec5694d365392d1bfdbcb2a0247d359d9cd9d18
Daniel Micay <danielmicay@gmail.com>
2023-05-03 12:56:33 -0400

    drop unnecessary com.android.library plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "8.0.1" apply false
-    id("com.android.library") version "8.0.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
 }
 
