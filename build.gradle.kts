e8e3ff9929d49cc4dd89649e9404534360665ae9
Daniel Micay <daniel.micay@grapheneos.org>
2023-07-03 19:46:16 -0400

    update Kotlin to 1.8.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 
 allprojects {
