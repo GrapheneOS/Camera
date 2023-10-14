2c6301648e69e5a4c94b91725829b9f192b2ba58
Pratyush <codelab@pratyush.dev>
2023-10-14 15:52:18 +0530

    convert if else to switch

--- app/src/main/java/app/grapheneos/camera/capturer/VideoCapturer.kt
+++ app/src/main/java/app/grapheneos/camera/capturer/VideoCapturer.kt
@@ -207,10 +207,14 @@ class VideoCapturer(private val mActivity: MainActivity) {
                     camConfig.mPlayer.playVRStopSound()
 
                     if (event.hasError()) {
-                        if (event.error == VideoRecordEvent.Finalize.ERROR_NO_VALID_DATA) {
-                            ctx.showMessage(R.string.recording_too_short_to_be_saved)
-                        } else {
-                            ctx.showMessage(ctx.getString(R.string.unable_to_save_video_verbose, event.error))
+                        when (event.error) {
+                            VideoRecordEvent.Finalize.ERROR_NO_VALID_DATA -> {
+                                ctx.showMessage(R.string.recording_too_short_to_be_saved)
+                            }
+
+                            else -> {
+                                ctx.showMessage(ctx.getString(R.string.unable_to_save_video_verbose, event.error))
+                            }
                         }
                         return@start
                     }
