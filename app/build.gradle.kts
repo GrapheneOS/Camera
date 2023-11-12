86c87eda68358a4c9906b887b58bfef7fd0bb112
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-12 18:07:00 -0500

    update Material Components library to 1.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     val cameraVersion = "1.3.0-rc02"
