cbe0b498574c792a89a00d10db9e41a649c218ed
Daniel Micay <daniel.micay@grapheneos.org>
2023-12-19 10:51:36 -0500

    update CameraX to 1.4.0-alpha03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    val cameraVersion = "1.4.0-alpha02"
+    val cameraVersion = "1.4.0-alpha03"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
