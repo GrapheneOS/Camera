d4003dc1c2553d34b8b6c63bf96db4501f93e12c
Daniel Micay <daniel.micay@grapheneos.org>
2024-03-01 13:35:54 -0500

    remove redundant Java/Kotlin version configuration
    This is now set automatically from the toolchain configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,15 +80,6 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_17)
-        targetCompatibility(JavaVersion.VERSION_17)
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
