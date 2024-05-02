b11887bca08281f67d803cddc9e5e7361131947e
Daniel Micay <daniel.micay@grapheneos.org>
2024-05-02 15:28:26 -0400

    update Material Components library to 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core:1.13.1")
 
