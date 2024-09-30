536e0d030c0f3a1c8996e10f772e93cb9964dbd6
Daniel Micay <daniel.micay@grapheneos.org>
2024-09-30 18:44:01 -0400

    minor cleanup

--- app/src/main/java/app/grapheneos/camera/util/BitmapUtils.kt
+++ app/src/main/java/app/grapheneos/camera/util/BitmapUtils.kt
@@ -9,17 +9,10 @@ import androidx.annotation.RequiresApi
 
 fun setBlurBitmapCompat(view: ImageView, bitmap: Bitmap, radius: Float = 4f) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        setBitmapWithBlurEffect(view, bitmap, radius)
+        val blurRenderEffect = RenderEffect.createBlurEffect(radius, radius, Shader.TileMode.CLAMP)
+        view.setImageBitmap(bitmap)
+        view.setRenderEffect(blurRenderEffect)
         return
     }
-
-    //this will eventually be removed
     view.setImageBitmap(app.grapheneos.camera.BlurBitmap[bitmap])
-}
-
-@RequiresApi(Build.VERSION_CODES.S)
-private fun setBitmapWithBlurEffect(view: ImageView, bitmap: Bitmap, radius: Float) {
-    val blurRenderEffect = RenderEffect.createBlurEffect(radius, radius, Shader.TileMode.CLAMP)
-    view.setImageBitmap(bitmap)
-    view.setRenderEffect(blurRenderEffect)
 }
