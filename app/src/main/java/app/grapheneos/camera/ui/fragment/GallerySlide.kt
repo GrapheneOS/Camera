ee459cdf7792fae50d754ef7d69ef240f16a6daa
MHShetty <shettymohitn@gmail.com>
2024-10-14 01:46:48 +0530

    Remove old unused code (related to views framework)

--- app/src/main/java/app/grapheneos/camera/ui/fragment/GallerySlide.kt
+++
@@ -1,8 +0,0 @@
-package app.grapheneos.camera.ui.fragment
-
-import androidx.recyclerview.widget.RecyclerView
-import app.grapheneos.camera.databinding.GallerySlideBinding
-
-class GallerySlide(val binding: GallerySlideBinding) : RecyclerView.ViewHolder(binding.root) {
-    @Volatile var currentPostion = 0
-}
