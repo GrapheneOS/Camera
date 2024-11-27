7c15b01b9e74bf128b2f7da290bae1206e5eb3dd
MHShetty <shettymohitn@gmail.com>
2024-11-27 11:10:08 +0530

    Improve gyroscope vibration implementation

--- app/src/main/java/app/grapheneos/camera/ui/activities/MainActivity.kt
+++ app/src/main/java/app/grapheneos/camera/ui/activities/MainActivity.kt
@@ -208,6 +208,14 @@ open class MainActivity : AppCompatActivity(),
 
     private var bottomNavigationBarPadding: Int = 0
 
+    private var shouldGyroVibrate = true
+    private var hasGyroVibrated = false
+
+    private val gyroVibRunnable = Runnable {
+        vibrateDevice()
+        hasGyroVibrated = true
+    }
+
     val thumbnailLoaderExecutor = Executors.newSingleThreadExecutor()
 
     private val runnable = Runnable {
@@ -1379,6 +1387,8 @@ open class MainActivity : AppCompatActivity(),
 
         private const val SWIPE_THRESHOLD = 100
         private const val SWIPE_VELOCITY_THRESHOLD = 100
+
+        private const val GYRO_VIBE_WAIT_TIME = 250L
     }
 
     override fun onDown(e: MotionEvent): Boolean {
@@ -1569,11 +1579,22 @@ open class MainActivity : AppCompatActivity(),
             if (gCircle.rotation != xAngle) {
                 gCircle.rotation = xAngle
                 gLineZ.rotation = xAngle
-                gAngleTextView.text = getString(R.string.degree_format, abs(xAngle).toInt())
+
+                val absXAngle = abs(xAngle).toInt()
+
+                gAngleTextView.text = getString(R.string.degree_format, absXAngle)
                 if (xAngle == 0f) {
                     setThicknessOfGLines(4)
-                    vibrateDevice()
+                    if (shouldGyroVibrate) {
+                        shouldGyroVibrate = false
+                        hasGyroVibrated = false
+                        handler.postDelayed(gyroVibRunnable, GYRO_VIBE_WAIT_TIME)
+                    }
                 } else {
+                    handler.removeCallbacks(gyroVibRunnable)
+                    if (!hasGyroVibrated || absXAngle > 5) {
+                        shouldGyroVibrate = true
+                    }
                     setThicknessOfGLines(2)
                 }
             }
