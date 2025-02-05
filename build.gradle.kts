e55625386f0c7b10a2d5517ba6e8c703ce9bf5da
Daniel Micay <daniel.micay@grapheneos.org>
2025-02-04 23:50:15 -0500

    update Kotlin to 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
 }
 
 allprojects {
