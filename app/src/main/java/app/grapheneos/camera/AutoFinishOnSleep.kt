af191e4b74bbddf79cdadbbc6ebbf111d8c22098
GrapheneOS <contact@grapheneos.org>
2025-02-09 12:30:38 -0500

    remove delay for finishing secure activities on screen off

--- app/src/main/java/app/grapheneos/camera/AutoFinishOnSleep.kt
+++ app/src/main/java/app/grapheneos/camera/AutoFinishOnSleep.kt
@@ -5,39 +5,25 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Handler
-import android.os.Looper
 import androidx.core.app.ActivityCompat
 
 // Finishes the passed [activity] and the ones present below it in the stack if the screen
-// turns off and the user is inactive for [WAITING_TIME_IN_MS] milliseconds
+// turns off
 class AutoFinishOnSleep(val activity: Activity) {
 
     companion object {
         private const val TAG = "AutoFinishOnSleep"
-        private const val WAITING_TIME_IN_MS = 1500L
 
         private val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_SCREEN_ON)
             addAction(Intent.ACTION_SCREEN_OFF)
         }
-    }
-
-    private val handler = Handler(Looper.getMainLooper())
-
-    private val runnable = Runnable {
-        ActivityCompat.finishAffinity(activity)
     }
 
     private val receiver = object: BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             when (intent?.action) {
                 Intent.ACTION_SCREEN_OFF -> {
-                    handler.postDelayed(runnable, WAITING_TIME_IN_MS)
-                }
-
-                Intent.ACTION_SCREEN_ON -> {
-                    handler.removeCallbacks(runnable)
+                    ActivityCompat.finishAffinity(activity)
                 }
             }
         }
