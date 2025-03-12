64526712e044816c47fcf2d20a09ae3afdabeebf
GrapheneOS <contact@grapheneos.org>
2025-03-12 15:41:50 -0400

    increment version to 84

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         applicationId = "app.grapheneos.camera"
         minSdk = 29
         targetSdk = 35
-        versionCode = 83
+        versionCode = 84
         versionName = versionCode.toString()
     }
 
