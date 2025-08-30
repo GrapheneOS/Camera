5ffee32d91dcc60b88a903da2e3d7274f38848d0
Daniel Micay <daniel.micay@grapheneos.org>
2025-08-29 23:05:51 -0400

    update Android Gradle plugin to 8.12.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.12.1" apply false
+    id("com.android.application") version "8.12.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
 }
 
