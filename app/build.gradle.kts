0bb05c0524a0520cdefd5c2b0f1377cdd41f0320
Daniel Micay <daniel.micay@grapheneos.org>
2025-08-30 18:40:40 -0400

    update CameraX to 1.5.0-rc01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.17.0")
 
-    val cameraVersion = "1.5.0-beta02"
+    val cameraVersion = "1.5.0-rc01"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
