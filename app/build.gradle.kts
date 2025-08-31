ca0a8ed6f4d76af8e35e1ec8e760f80c3ab77789
Daniel Micay <daniel.micay@grapheneos.org>
2025-08-31 18:49:16 -0400

    increment version to 89

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         applicationId = "app.grapheneos.camera"
         minSdk = 29
         targetSdk = 35
-        versionCode = 88
+        versionCode = 89
         versionName = versionCode.toString()
     }
 
