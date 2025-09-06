bcf29da4250d6a40a8a7e118c7a628d35f33f78a
MHShetty <shettymohitn@gmail.com>
2025-09-06 17:19:05 +0530

    Update material components library to 1.13.0
    From version 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.17.0")
 
