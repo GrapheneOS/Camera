675c14419bb4ee62ef3fd8e25d8f10b52f81a0e7
MHShetty <shettymohitn@gmail.com>
2025-11-02 05:46:41 +0530

    Update CameraX to 1.6.0-alpha01 (from version 1.5.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.17.0")
 
-    val cameraVersion = "1.5.0"
+    val cameraVersion = "1.6.0-alpha01"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
