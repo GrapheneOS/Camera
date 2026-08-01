0a88f7330761d3156389d556e9b24e9e9cd2629e
Daniel Micay <daniel.micay@grapheneos.org>
2026-08-01 18:02:31 -0400

    increment version to 94

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "app.grapheneos.camera"
         minSdk = 29
         targetSdk = 37
-        versionCode = 93
+        versionCode = 94
         versionName = versionCode.toString()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
