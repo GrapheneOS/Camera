948ed3e0bce9e44cf53cc42ce840f3eac19f501a
Matvei Plokhov <mpplokho@gmail.com>
2026-09-17 18:19:12 +0200

    Fix detekt and ktlint findings

--- app/src/test/java/app/grapheneos/camera/ui/viewfinder/screen/delegate/ViewfinderModeDelegateTest.kt
+++ app/src/test/java/app/grapheneos/camera/ui/viewfinder/screen/delegate/ViewfinderModeDelegateTest.kt
@@ -5,8 +5,8 @@ import app.grapheneos.camera.ui.viewfinder.screen.ViewfinderStateHolder
 import app.grapheneos.camera.ui.viewfinder.screen.model.ViewfinderState
 import app.grapheneos.camera.ui.viewfinder.screen.model.ViewfinderUiState
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
